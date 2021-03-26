75120390193e4a0d76b7ec10992b68e772b4b132
jaewooklim1 <jaewooklim1@gmail.com>
2021-03-26 15:31:09 -0400

    finished 02-queries

--- Movie Buff/movie_buff/02_queries.rb
+++ Movie Buff/movie_buff/02_queries.rb
@@ -3,16 +3,25 @@ def eighties_b_movies
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
 
+  Movie.select(:id, :title, :yr, :score).where(yr: 1980..1989, score: 3..5)
+
 end
 
 def bad_years
-  # List the years in which a movie with a rating above 8 was not released.
+  # List the years in which a movie with a rating above 8 was not released.  
+
+  Movie.group(:yr).having("MAX(score) < 8").pluck(:yr)
 
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
+
+  Actor
+    .select(:id, :name)
+    .from(title)
+    .order('castings.ord')
 
 end
 
@@ -23,10 +32,23 @@ def vanity_projects
 
   # Note: Directors appear in the 'actors' table.
 
+  Movie
+    .select(:title, :id, actors.name)
+    .joins(:actors)
+    .where('director_id = actors.id')
+    .joins(:castings)
+    .where('ord = 1')
+
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
+
+  Actor
+    .select(:name, :id, 'COUNT(castings.actor_id)')
+    .joins(:castings)
+    .where.not(castings: { ord: 1 })
+    .limit(2)
 
 end
